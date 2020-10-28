e08de034273402c73479f9c7071045107954ac2e
Joao Mascarenhas <up201806389@fe.up.pt>
2020-10-28 11:00:54 +0000

    initializing tp2

---
+++ TP2/MyInterface.js
@@ -0,0 +1,84 @@
+/**
+* MyInterface class, creating a GUI interface.
+*/
+class MyInterface extends CGFinterface {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Initializes the interface.
+     * @param {CGFapplication} application
+     */
+    init(application) {
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        //  http://workshop.chromeexperiments.com/examples/gui
+
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        
+        this.gui.add(this.scene, 'zoom', 0.1, 8).name('Zoom');
+
+        // add a group of controls (and open/expand by defult)
+
+        this.initKeys();
+
+        return true;
+    }
+
+    /**
+     * initKeys
+     */
+    initKeys() {
+        this.scene.gui=this;
+        this.processKeyboard=function(){};
+        this.activeKeys={};
+    }
+
+    processKeyDown(event) {
+        this.activeKeys[event.code]=true;
+    };
+
+    processKeyUp(event) {
+        this.activeKeys[event.code]=false;
+    };
+
+    isKeyPressed(keyCode) {
+        return this.activeKeys[keyCode] || false;
+    }
+
+    /**
+     * Adds lights to the User Interface
+     */
+    addLightsGUI(){
+        var keyNames=Object.keys(this.scene.graph.lights);
+
+        var lightsFolder=this.gui.addFolder('Lights');
+
+        lightsFolder.open();
+
+        for(let i= 0; i<keyNames.length;i++)
+            lightsFolder.add(this.scene.lights[i],'enabled').name(keyNames[i]); 
+    }
+
+    /**
+     * Adds cameras to the User Interface
+     */
+    addCamerasGUI(){ 
+        var viewsFolder = this.gui.addFolder('Views');
+        viewsFolder.open();
+
+        const viewArray = Object.keys(this.scene.graph.views);
+
+        this.currView = this.scene.graph.defaultView;
+
+        viewsFolder.add(this, 'currView', viewArray).name('Camera').onChange(val => this.scene.chooseView(val));
+
+    }
+}
