06787264fdeee5be8308fbc8315086be84fa5ba8
João Vilhena <joao.mascarenhas2000@gmail.com>
2020-11-18 01:25:23 +0000

    patch(generic primitive for curved surfaces) done i think

---
+++ TP2/primitives/MyPatch.js
@@ -0,0 +1,37 @@
+class Patch extends CGFobject{//primitiva generica para produzir superficies curvas a partir do numero de pontos,partes e dos pontos de controlo
+
+
+
+    constructor(scene,npartsU,npartsV,npointsU,npointsV,controlPoints){
+        super(scene);
+        this.npointsU = npointsU;
+        this.npointsV = npointsV;
+        this.npartsU = npartsU;
+        this.npartsV = npartsV;
+        this.controlPoints = controlPoints;
+        this.initBuffers();
+       
+    }
+
+        initBuffers() {             //grau=nº pontos - 1
+          let nurbsSurface = new CGFnurbsSurface(this.npointsU-1,this.npointsV-1,this.controlPoints);
+
+          this.nurbsObj = new CGFnurbsObject(this.scene,this.npointsU,this.npointsV,nurbsSurface);
+          
+        }
+
+      
+        display() {
+          this.nurbsObj.display();
+        }
+
+
+
+
+
+        updateTexCoords(Sfactor,Tfactor) {
+            this.updateTexCoordsGLBuffers();
+        }
+      }
+
+    
