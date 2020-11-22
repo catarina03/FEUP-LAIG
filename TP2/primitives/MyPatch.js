993116f26f09e78bd6db50ab506b2cf30c7d226c
Catarina <up201806610@fe.up.pt>
2020-11-22 22:34:54 +0000

    MyPatch bug fixed

--- TP2/primitives/MyPatch.js
+++ TP2/primitives/MyPatch.js
@@ -1,14 +1,8 @@
 /**
  * MyPatch class, which represents a Patch object
  */
-
-  
-
-
-
-class Patch extends CGFobject{//primitiva generica para produzir superficies curvas a partir do numero de pontos,partes e dos pontos de controlo
-
-/**
+class MyPatch extends CGFobject{    //primitiva generica para produzir superficies curvas a partir do numero de pontos,partes e dos pontos de controlo
+  /**
    * @constructor
    * @param {XMLScene} scene           represents the CGFscene
    * @param {number}   nPartsU         
@@ -17,38 +11,32 @@ class Patch extends CGFobject{//primitiva generica para produzir superficies cur
    * @param {number}   nPointsV        control points V
    * @param {array}    controlPoints   Array with ControlPoints 
    */
+  constructor(scene,npartsU,npartsV,npointsU,npointsV,controlPoints){
+    super(scene);
+    this.npointsU = npointsU;
+    this.npointsV = npointsV;
+    this.npartsU = npartsU;
+    this.npartsV = npartsV;
+    this.controlPoints = controlPoints;
+    this.initBuffers();
+  }
 
 
-    constructor(scene,npartsU,npartsV,npointsU,npointsV,controlPoints){
-        super(scene);
-        this.npointsU = npointsU;
-        this.npointsV = npointsV;
-        this.npartsU = npartsU;
-        this.npartsV = npartsV;
-        this.controlPoints = controlPoints;
-        this.initBuffers();
-       
-    }
-
-        initBuffers() {             //grau=nº pontos - 1
-          let nurbsSurface = new CGFnurbsSurface(this.npointsU-1,this.npointsV-1,this.controlPoints);
-
-          this.nurbsObj = new CGFnurbsObject(this.scene,this.npartsU,this.npartsV,nurbsSurface);
-          
-        }
+  initBuffers() {             //grau=nº pontos - 1
+    let nurbsSurface = new CGFnurbsSurface(this.npointsU-1,this.npointsV-1,this.controlPoints);
+    this.nurbsObj = new CGFnurbsObject(this.scene,this.npartsU,this.npartsV,nurbsSurface);
+  }
 
       
-        display() {
-          this.nurbsObj.display();
-        }
+  display() {
+    this.nurbsObj.display();
+  }
 
 
+  updateTexCoords(Sfactor,Tfactor) {
+      this.updateTexCoordsGLBuffers();
+  }
 
-
-
-        updateTexCoords(Sfactor,Tfactor) {
-            this.updateTexCoordsGLBuffers();
-        }
-      }
+}
 
     
